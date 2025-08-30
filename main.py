a0d656dab09067842362b56a03015a0880d3823e
= <elkarafli.taha@gmail.com>
2025-08-30 20:54:29 +0200

    feature:add treatement and writing

--- main.py
+++ main.py
@@ -21,16 +21,13 @@ for df, cols in [(yields, ["ts_start","ts_end"]), (events, ["ts_start","ts_end"]
 
 from pyspark.sql import functions as F
 
-# Aliases
 y = yields.alias("y")
 s = static_info.alias("s")
 e = events.alias("e")
 m = median_ref.alias("m")
 
-# 1) Join yields + static_info
 df = y.join(s, "logical_device_mrid", "left")
 
-# 2) Join events par chevauchement réel (intervalle qui s'intersecte)
 df = df.join(
     e,
     on=(
@@ -41,20 +38,16 @@ df = df.join(
     how="left"
 )
 
-# 2bis) IMPORTANT : ne garde qu'une seule version des colonnes ambiguës
-# Ici on garde toutes les colonnes de yields (y.*), et seulement les colonnes utiles des autres tables
 df = df.select(
-    F.col("y.*"),                                  # ts_start/ts_end = version yields
+    F.col("y.*"),                                  
     F.col("s.inverter_function_type"),
     F.col("s.storage_inverter_type"),
     F.col("s.ac_max_power"),
     F.col("e.iec63019_category_id")
 )
 
-# 3) Tu peux maintenant dériver 'date' sans ambiguïté
 df = df.withColumn("date", F.to_date(F.col("ts_start")))
 
-# 4) Join avec la ref journalière (on forme 'date' côté ref aussi)
 m_day = m.select(
     "project_code",
     F.to_date("ts_start").alias("date"),
@@ -62,7 +55,6 @@ m_day = m.select(
 )
 df = df.join(m_day, on=["project_code","date"], how="left")
 
-# 5) Suite du traitement…
 df = df.withColumn(
     "potential_production",
     F.col("specific_yield_ac") * F.col("ac_max_power") * F.lit(1/6.0)
